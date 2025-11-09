c6d7ded51f8f576b74549adc6dfc932252889ca4
lels-ubuntu <francis.jore..canuto@gmail.com>
2025-11-10 02:57:03 +0800

    add kafka compose

---
+++ kafka/config/topics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Creating topics in Kafka..."
+
+docker exec kafka kafka-topics --create --topic api-logs --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1 --if-not-exists
+docker exec kafka kafka-topics --create --topic repository-logs --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1 --if-not-exists
+
+echo "Topics created successfully!"
