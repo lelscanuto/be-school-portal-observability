1e6896620d4f80078844f203fe104d812ad84f42
lels-ubuntu <francis.jore..canuto@gmail.com>
2025-11-26 20:28:16 +0800

    add promtail, otel-collector, promentheus, grafana

--- kafka/config/topics.sh
+++ kafka/config/topics.sh
@@ -2,7 +2,7 @@
 
 echo "Creating topics in Kafka..."
 
-docker exec kafka kafka-topics --create --topic api-logs --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1 --if-not-exists
-docker exec kafka kafka-topics --create --topic repository-logs --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1 --if-not-exists
+docker exec kafka kafka-topics --create --topic api-logs --bootstrap-server kafka:29092 --partitions 3 --replication-factor 1 --if-not-exists
+docker exec kafka kafka-topics --create --topic repository-logs --bootstrap-server kafka:29092 --partitions 3 --replication-factor 1 --if-not-exists
 
 echo "Topics created successfully!"
